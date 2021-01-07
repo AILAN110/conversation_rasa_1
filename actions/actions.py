75a8a22a58518ee65e80df47f5055280fee2076f
AILAN110 <1032163939@qq.com>
2021-01-07 17:00:02 +0800

    rasa_1 updated

--- actions/actions.py
+++ actions/actions.py
@@ -7,10 +7,10 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
+from typing import Any, Text, Dict, List
+from rasa_sdk.forms import FormAction
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
 #
 #
 # class ActionHelloWorld(Action):
@@ -25,3 +25,31 @@
 #         dispatcher.utter_message(text="Hello World!")
 #
 #         return []
+
+class WeatherForm(FormAction):
+
+    def name(self) -> Text:
+        """Unique identifier of the form"""
+
+        return "travel_form"
+
+    @staticmethod
+    def required_slots(tracker: Tracker) -> List[Text]:
+        """A list of required slots that the form has to fill"""
+
+        return ["city"]
+
+    def submit(
+            self,
+            dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any],
+    ) -> List[Dict]:
+        """Define what the form has to do
+            after all required slots are filled"""
+        city = tracker.get_slot('city')
+        #查接口。。。。---->travel_list
+        travel_list=["欢乐谷","世界之窗"]
+        travel_data ="{}都有一下好玩的地方：{}".format(city,travel_list)
+        dispatcher.utter_message(travel_data)
+        return []
